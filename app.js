ccf1170a85f9239625ca2d9a308253284f8cd4ec
emaenriquez <emaenriquez1985@gmail.com>
2023-10-13 20:38:03 -0300

    nuevos archivos modificados

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 const imageInput = document.getElementById('imageInput');
-        const convertButton = document.getElementById('convertButton');
-        const downloadButton = document.getElementById('downloadButton');
-        const imageContainer = document.getElementById('imageContainer');
-        const imagePreview = document.getElementById('imagePreview');
+const convertButton = document.getElementById('convertButton');
+const downloadButton = document.getElementById('downloadButton');
+const imageContainer = document.getElementById('imageContainer');
+const imagePreview = document.getElementById('imagePreview');
+let imageData = null;
 
-        let imageData = null;
-
-        imageInput.addEventListener('change', function () {
-            const selectedFile = imageInput.files[0];
-            const reader = new FileReader();
-
-            reader.onload = function () {
-                imageData = reader.result;
-                imagePreview.src = imageData;
-
-                imageContainer.style.display = 'block';
-                downloadButton.style.display = 'block';
-            };
-
-            reader.readAsDataURL(selectedFile);
-        });
-
-        downloadButton.addEventListener('click', function () {
-            if (imageData) {
-                const pdf = new jsPDF();
-                pdf.addImage(imageData, 'JPEG', 10, 10, 100, 100); // Ajusta las coordenadas y dimensiones según tus necesidades
-                pdf.save('imagen_a_pdf.pdf');
-            }
-        });
+imageInput.addEventListener('change', function () {
+    const selectedFile = imageInput.files[0];
+    const reader = new FileReader();
+    reader.onload = function () {
+        imageData = reader.result;
+        imagePreview.src = imageData;
+        imageContainer.style.display = 'block';
+        downloadButton.style.display = 'block';
+    };
+    reader.readAsDataURL(selectedFile);
+});
+downloadButton.addEventListener('click', function () {
+    if (imageData) {
+        const pdf = new jsPDF();
+        pdf.addImage(imageData, 'JPEG', 10, 10, 100, 100); // Ajusta las coordenadas y dimensiones según tus necesidades
+        pdf.save('imagen_a_pdf.pdf');
+    }
+});
